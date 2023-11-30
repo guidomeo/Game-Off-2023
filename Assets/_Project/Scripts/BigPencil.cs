c7098cc1a6a5be8602e064496225a924c2bd3dae
Guido Meo <guido.meo@gmail.com>
2023-11-30 23:21:39 +0100

    Pencil Altar

--- Assets/_Project/Scripts/BigPencil.cs
+++ Assets/_Project/Scripts/BigPencil.cs
@@ -8,10 +8,16 @@ public class BigPencil : MonoBehaviour
 {
     [SerializeField] private float animSpeed;
     [SerializeField] private SpriteRenderer glow;
+    [SerializeField] private Transform pencilAnim;
 
+    //private Sequence loopAnim;
     private void Awake()
     {
         glow.color = new Color(1f,1f,1f, 0f);
+        //loopAnim = DOTween.Sequence();
+        //loopAnim.Append(pencilAnim.DOLocalMove(new Vector3(-0.139f, 0.196f), 1f));
+        //loopAnim.Append(pencilAnim.DOLocalMove(Vector3.zero, 1f));
+        //loopAnim.SetLoops(-1);
     }
 
     private void OnTriggerEnter2D(Collider2D col)
@@ -24,11 +30,22 @@ public class BigPencil : MonoBehaviour
         Transform parent = transform.parent;
 
         player.cc.canMove = false;
+        //parent.GetComponent<SpriteRenderer>().sortingLayerName = "Player";
+
+        //loopAnim.Kill();
+        //Destroy(pencilAnim.GetComponent<Animator>());
+        Sequence anim = DOTween.Sequence();
+        //anim.Append(pencilAnim.DOLocalMove(Vector3.zero, 0.1f));
+        //anim.Join(pencilAnim.DOLocalRotate(Vector3.zero, 0.1f));
+        //anim.Append(pencilAnim.DOLocalRotate(Vector3.forward * 30f, 0.5f));
+        //anim.Append(pencilAnim.DOLocalRotate(Vector3.forward * -30f, 1f));
+        //anim.Append(pencilAnim.DOLocalRotate(Vector3.forward * 30f, 0.2f));
+        //anim.Append(pencilAnim.DOLocalRotate(Vector3.forward * -30f, 0.4f));
+        anim.Append(pencilAnim.DOLocalRotate(Vector3.forward * 0f, 0.1f));
         
-        glow.DOFade(1f, 1.5f).OnComplete(() =>
+        anim.OnComplete(() =>
         {
             player.PickBigPencil(parent, animSpeed);
-            parent.GetComponent<SpriteRenderer>().sortingLayerName = "Player";
             
             glow.DOFade(0f, 1.5f).SetDelay(3.5f);
         });
